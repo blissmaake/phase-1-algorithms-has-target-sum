d642338a1f16625b5b76d973c872471d6b1d1174
Bliss <blissmaake140@gmail.com>
2022-12-12 08:38:18 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) 
+      return true;
+    }
+  }
+
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -12,6 +22,10 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+  collection of an Array and a target value are inserted as arguments 
+  [a,b,c,d,e] then we iterate through numbers adding a to b then c etc... and comparing sum to === the target value 
+  if === target value return true
+  else !== return false
 */
 
 // You can run `node index.js` to view these console logs
